7e2047272edfe467dc85671bf4b91991e43dcd41
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-04-09 03:15:30 -0700

    Postorder traversale3

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder  - postorder traversal
+ * @tree: a tree
+ * @func: a function pointer
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree != NULL && func != NULL)
+{
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+func(tree->n);
+}
+}
